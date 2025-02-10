65784d27ec0023ef97f1521b31c27d5eb2ea8722
Bruno Alves <93.bruno@gmail.com>
2025-02-10 15:46:12 +0000

    created database connection

--- server/server.js
+++ server/server.js
@@ -3,11 +3,16 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 
+import connectDB from "./config/mongodb.js";
+
 const app = express();
 const port = process.env.PORT || 4000;
+connectDB();
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credential: true }));
+
+app.get("/", (req, res) => res.send("API Working"));
 
 app.listen(port, () => console.log(`Server started on PORT:${port}`));
