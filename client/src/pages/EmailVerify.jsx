23ac498cb893b61b48dce79a9d5a76011037bdb8
Bruno Alves <93.bruno@gmail.com>
2025-05-14 16:21:25 +0100

    Redirect user to homepage if already logged in and account is verified

--- client/src/pages/EmailVerify.jsx
+++ client/src/pages/EmailVerify.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { assets } from "../assets/assets";
 import axios from "axios";
 import { AppContext } from "../context/AppContext";
@@ -61,6 +61,10 @@ const EmailVerify = () => {
       toast.error(error.message);
     }
   };
+
+  useEffect(() => {
+    isLoggedin && userData && userData.isAccountVerified && navigate("/");
+  }, [isLoggedin, userData]);
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400">
