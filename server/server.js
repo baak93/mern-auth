9c7cfab4dbf00faf9f0f209d3c7f48e5fa3794d0
Bruno Alves <93.bruno@gmail.com>
2025-05-19 17:47:03 +0100

    add Vercel domain in allowedOrigins

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://mern-auth-brown-ten.vercel.app/",
+];
 
 app.use(express.json());
 app.use(cookieParser());
